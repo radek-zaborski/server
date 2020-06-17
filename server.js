5932460edaecbab8d93648b8a529c7fb59dd77b6
Radoslaw Zaborski <radex171@gmail.com>
2020-06-17 18:20:10 +0200

    for publish

--- server.js
+++ server.js
@@ -1,24 +1,31 @@
-const express = require ('express');
-const cors = require ('cors');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+
 const testimonialsRoutes = require('./routes/testimonials.routes');
-const concerts = require('./routes/concerts.routes');
-const seats = require('./routes/seats.routes');
-const db = require ('./db');
+const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
 
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname + '/client/build')));
+
 app.use(cors());
+app.use('/api/', testimonialsRoutes);
+app.use('/api/', concertsRoutes);
+app.use('/api/', seatsRoutes);
 
-app.use( testimonialsRoutes);
-app.use( concerts);
-app.use( seats);
-
-app.use((req, res) => {
-    res.status(404).json({ message: 'not found... 404' });
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-app.listen(8000, () => {
-    console.log('Server is running on port: 8000');
+app.use((req, res) => {
+  res.status(404).json({ message: 'Oops... Not found...' });
+});
+
+app.listen(process.env.PORT || 8000, () => {
+  console.log('Server is running on port: 8000');
 });
